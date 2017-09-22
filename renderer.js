dd60fec42e3ea9077f6ccfad5b140dca3605b214
Wojciech Olejnik <divoolej@gmail.com>
2017-09-22 14:09:39 +0200

    Add SCSS support and compilation flow

--- renderer.js
+++ renderer.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './js/components/App.jsx'
 
+import './css/application.scss'
+
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "renderer_bundle.css",
+  disable: process.env.NODE_ENV === "development",
+});
 
 module.exports = {
   entry: './renderer.js',
@@ -11,6 +17,13 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, use: extractSass.extract({
+        use: ['css-loader', 'sass-loader'],
+        fallback: 'style-loader',
+      })}
     ]
-  }
+  },
+  plugins: [
+    extractSass,
+  ]
 }

a21ed7244af6589438a08a3484bc98551451b52f
Wojciech Olejnik <divoolej@gmail.com>
2017-09-22 21:07:41 +0200

    Adjust window size

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1280, height: 600})
+  win = new BrowserWindow({width: 1280, height: 622})
 
   // and load the index.html of the app.
   win.loadURL(url.format({

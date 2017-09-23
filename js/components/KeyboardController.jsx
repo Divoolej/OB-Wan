9f3203908c5cced7d25ebe020edf3ae842544823
Wojciech Olejnik <divoolej@gmail.com>
2017-09-23 19:35:04 +0200

    Add a KeyboardController component

---
+++ js/components/KeyboardController.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class KeyboardControllerComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.pressedKeys = {}
+    this.setEventListeners()
+    this.noteMap = {
+      a: 'C4', w: 'C#4', s: 'D4', e: 'D#4', d: 'E4', f: 'F4', t: 'F#4', g: 'G4',
+      y: 'G#4', h: 'A4', u: 'A#4', j: 'B4', k: 'C5', o: 'C#5', l: 'D5', p: 'D#5',
+    }
+  }
+
+  onKeyDown = (event) => {
+    if (this.pressedKeys[event.key] !== true) {
+      this.pressedKeys[event.key] = true
+      const note = this.noteMap[event.key]
+      if (note) {
+        this.props.noteOn(note)
+      }
+    }
+  }
+
+  onKeyUp = (event) => {
+    this.pressedKeys[event.key] = false
+    const note = this.noteMap[event.key]
+    if (note) {
+      this.props.noteOff(note)
+    }
+  }
+
+  setEventListeners = () => {
+    document.addEventListener('keydown', this.onKeyDown)
+    document.addEventListener('keyup', this.onKeyUp)
+  }
+
+  render() { return null }
+}
+
+KeyboardControllerComponent.propTypes = {
+  noteOn: PropTypes.func.isRequired,
+  noteOff: PropTypes.func.isRequired,
+}
+
+import { connect } from 'react-redux'
+import { noteOn, noteOff } from '../actions/instrument-actions.js'
+
+const actions = { noteOn, noteOff }
+
+const KeyboardControllerContainer = connect(null, actions)(KeyboardControllerComponent)
+
+export default KeyboardControllerContainer

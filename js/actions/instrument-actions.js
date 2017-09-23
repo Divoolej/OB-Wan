b831f7e55c1fed769b1b1a0a6cb6c3bf3719c4da
Wojciech Olejnik <divoolej@gmail.com>
2017-09-23 19:33:05 +0200

    Add instrument actions

---
+++ js/actions/instrument-actions.js
@@ -0,0 +1,39 @@
+import Tone from 'tone'
+
+export const toggleInstrumentEnabled = () => ({
+  type: 'INSTRUMENT_TOGGLE_ENABLED',
+  payload: {},
+})
+
+export const changeInstrument = (instrumentName) => {
+  let synth = null
+  switch (instrumentName) {
+    case 'Synth':
+      synth = new Tone.Synth();
+      break;
+    case 'PolySynth':
+      synth = new Tone.PolySynth();
+      break;
+    case 'MonoSynth':
+      synth = new Tone.MonoSynth({ volume: -20 });
+      break;
+  }
+  return {
+    type: 'INSTRUMENT_CHANGE_INSTRUMENT_TYPE',
+    payload: {
+      type: instrumentName,
+      synth: synth,
+    }
+  }
+}
+
+export const noteOn = (note) => ({
+  type: 'INSTRUMENT_NOTE_ON',
+  payload: { note },
+})
+
+export const noteOff = (note) => ({
+  type: 'INSTRUMENT_NOTE_OFF',
+  payload: { note },
+})
+

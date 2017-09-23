a4b37966e338ce637d4a41331e175ebb7e68405e
Wojciech Olejnik <divoolej@gmail.com>
2017-09-23 19:34:27 +0200

    Add an empty Synth component

---
+++ js/components/instrument/Synth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Synth = ({}) => (
+  <div className="Synth">
+    SYNTH
+  </div>
+)
+
+Synth.propTypes = {
+
+}
+
+export default Synth;

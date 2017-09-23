de2da2b4ba91d73eaa1e715351b569dd557f004a
Wojciech Olejnik <divoolej@gmail.com>
2017-09-23 19:35:12 +0200

    Render the Instrument in the Rack

--- js/components/Rack.jsx
+++ js/components/Rack.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Instrument from './instrument/Instrument.jsx'
+
 export default class Rack extends React.Component {
   render() {
     return(
       <div className="Rack">
+        <Instrument />
       </div>
     )
   }
